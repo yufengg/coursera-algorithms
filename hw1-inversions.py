8b4055aa0484198fe58ae75b1f5f93dbdbb8c570
yufengg <g.yufeng@gmail.com>
2013-07-13 01:12:22 -0400

    working merge algorithm

--- hw1-inversions.py
+++ hw1-inversions.py
@@ -1,1 +1,28 @@
-print "Hello world!" 
+
+def mergeArray(A , B):
+	print "Hello world!" 
+	C = []
+	total = len(A) + len(B)
+	aIdx = 0
+	bIdx = 0
+	for k in range(total):
+		print k, aIdx, bIdx, C
+		if aIdx == len(A):
+			C.append(B[bIdx])
+		elif bIdx == len(B):
+			C.append(A[aIdx])
+		else:
+			if A[aIdx] < B[bIdx]:
+				C.append(A[aIdx])
+				aIdx += 1
+			else:
+				print B[bIdx]
+				C.append(B[bIdx])
+				bIdx += 1
+	return C
+
+
+listA = [2,4,6,7]
+listB = [1,3,5,8]
+listC = mergeArray(listA, listB)
+print listC
