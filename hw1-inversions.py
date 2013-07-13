fd1aeb06910a90f2931edc36823f1350095a102f
yufengg <g.yufeng@gmail.com>
2013-07-13 13:12:58 -0400

    now can read files into a list

--- hw1-inversions.py
+++ hw1-inversions.py
@@ -6,7 +6,7 @@ def mergeArray(A , B):
 	aIdx = 0
 	bIdx = 0
 	for k in range(total):
-		print k, aIdx, bIdx, C
+		#print k, aIdx, bIdx, C
 		if aIdx == len(A):
 			C.append(B[bIdx])
 		elif bIdx == len(B):
@@ -16,13 +16,38 @@ def mergeArray(A , B):
 				C.append(A[aIdx])
 				aIdx += 1
 			else:
-				print B[bIdx]
 				C.append(B[bIdx])
 				bIdx += 1
 	return C
 
+def readFile(filename, l):
+	# f = open(filename, "r")
+	with open(filename, "r") as f:
+		for line in f:
+			l.append(int(line))
+			# print f.readline()
 
-listA = [2,4,6,7]
-listB = [1,3,5,8]
-listC = mergeArray(listA, listB)
-print listC
+	if f.closed == False:
+		f.close()
+
+def main ():
+	listA = [2,4,6,7]
+	listB = [1,3,5,8]
+	listC = mergeArray(listA, listB)
+	print listC
+
+# this is just a test
+	testInts = []
+	readFile("test.txt", testInts)
+	print testInts
+	print testInts[1:2]
+
+# get the big one
+	allInts = []
+	readFile("IntegerArray.txt", allInts)
+	print allInts[:15]
+	print len(allInts)
+
+	return 0
+
+main()
